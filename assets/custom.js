f0f761d4614013f5c2fb6257077d895e06c1d096
lenmuel <lenmuel.dlrs@gmail.com>
2025-03-02 19:16:22 +0800

    Adding functionality for play button in image with text

---
+++ assets/custom.js
@@ -0,0 +1,22 @@
+(function() {
+    let videoWithText = document.querySelectorAll('.video__with-text');
+
+    if( videoWithText.length > 0 ){
+        videoWithText.forEach((viditem)=>{
+            let btnContainer = viditem.querySelector('.video__btncontainer');
+            let playBtn = btnContainer.querySelector('.video__playbutton');
+            let video = viditem.querySelector('video');
+
+            playBtn.addEventListener('click', () => {
+                playBtn.style.opacity = playBtn.style.opacity === '0' ? '1' : '0';
+                video.paused ? video.play() : video.pause();
+            });
+
+            btnContainer.addEventListener('click', (event) => {
+                if (event.target !== playBtn) {
+                    playBtn.click();
+                }
+            });
+        })
+    }
+})();
